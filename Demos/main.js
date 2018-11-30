c82dd81a214e79934c6466e084a438afb33a3ec7
Carsten Ruhl <ToxicGecko13@Gmail.com>
2018-11-30 14:52:22 -0800

    Sec Commit - sec fix

--- Demos/main.js
+++ Demos/main.js
@@ -1,13 +1,15 @@
 // This code starts to run after the HTML page is loaded.
 // Here is our array of board games in order
 
-let boardGames = "['Catan', 'Pandemic', 'Ticket to Ride']";
-console.log(boardGames.langth)
+let boardGames = ['Catan', 'Pandemic', 'Ticket to Ride'];
+
+console.log(boardGames.langth);
 
 // Something wrong here, it needs fixing.
-// for (let count = 0; count < boardGames.length; count++) {  
-//     // console.log only shows up in the Dev Tools, so use it to figure out what is wrong.
-//     console.log(boardGames[count]);
-//     // console.log(document.getElementById(count.toString()).innerHTML);
-//     // document.getElementById(count.toString()).innerHTML = boardGames[count];
-// };
+for (let count = 0; count < boardGames.length; count++) {  
+    // console.log only shows up in the Dev Tools, so use it to figure out what is wrong.
+    // console.log(boardGames[count]);
+    console.log(count.toString())
+    // console.log(document.getElementById(count.toString()).innerHTML);
+    document.getElementById(count.toString()).textContent = boardGames[count];
+};
